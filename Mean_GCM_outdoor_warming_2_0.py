c0af21d203dd6567427bd31866370061e6df0601
nicolenicolen <61506119+nicolenicolen@users.noreply.github.com>
2021-05-21 10:52:06 +0200

    Add files via upload

---
+++ Mean_GCM_outdoor_warming_2_0.py
@@ -0,0 +1,36 @@
+import xarray as xr
+import os,sys,glob
+import numpy as np
+import pandas as pd
+import os
+import psutil
+process = psutil.Process(os.getpid())
+
+# Directive on PIK Cluster + example dataset 
+
+os.chdir('/p/tmp/vmaanen/new/Labour_Productivity/GCM_summary_warming/outdoor/')
+
+example_nc = xr.open_dataset('Impact_GFDL-ESM2M_rcp85_r1i1p1_EWEMBI_landonly.nc4')
+
+
+models = ['GFDL-ESM2M', 'IPSL-CM5A-LR','HadGEM2-ES','MIROC5']
+impacts = ['Impact']
+coordinates = dict(lat=example_nc.lat, lon=example_nc.lon, model=models, impact=impacts)
+data = xr.DataArray(data = np.zeros([len(impacts),len(models),len(example_nc.lat),len(example_nc.lon)])*np.nan,
+                    coords = coordinates,
+                    dims = ['impact','model','lat','lon'])
+
+#print(coordinates)
+#print(data)
+
+for impact in impacts:
+        for model in models:
+
+            print(xr.open_dataset(impact+'_'+model+'_rcp85_r1i1p1_EWEMBI_landonly.nc4'))
+            data.loc[impact,model,:,:] = xr.open_dataset(impact+'_'+model+'_rcp85_r1i1p1_EWEMBI_landonly.nc4')['Impact_2_0'] 
+
+xr.Dataset({'Impact_2_0':data}).to_netcdf('/p/tmp/vmaanen/new/Labour_Productivity/Summary_warming/outdoor/Impact_2_0_LP_all_GCMs_warming.nc4')
+
+
+# mean over models:
+#data.mean(dim='model').mean(dim='impact').loc[1.5]
